cbe8d403d7c5fbe1b9dcc4c2a0064f3b6c88f06d
Kirk Gilmore <kirklgilmore@gmail.com>
2024-06-08 12:19:30 -0700

    add missing files

---
+++ internal/clients/dnd5e/interface.go
@@ -0,0 +1,8 @@
+package dnd5e
+
+import "github.com/KirkDiggler/dnd-bot-api/internal/entities"
+
+type Client interface {
+	ListClasses() ([]*entities.Class, error)
+	ListRaces() ([]*entities.Race, error)
+}
